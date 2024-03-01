c9616541cf91334a5507bbdab3d3588fdd6f7820
DimAlus <dima.zvyagin.00@mail.ru>
2024-03-01 21:22:52 +0300

    changes: add tables for TileTypes; add LgService Log

--- Source/GBTeam6/Lib/Typing.cpp
+++ Source/GBTeam6/Lib/Typing.cpp
@@ -1,3 +1,4 @@
 #include "./Typing.h"
 
 DEFINE_LOG_CATEGORY(LgPlayer);
+DEFINE_LOG_CATEGORY(LgService);
--- Source/GBTeam6/Lib/Typing.h
+++ Source/GBTeam6/Lib/Typing.h
@@ -7,6 +7,7 @@
 #include "Typing.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LgPlayer, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LgService, Log, All);
 
 UCLASS()
 class GBTEAM6_API UTyping : public UBlueprintFunctionLibrary {
@@ -46,4 +47,38 @@ public:
 	/** Command input action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
 	UInputAction* CommandAction {nullptr};
+};
+
+
+UENUM(BlueprintType)
+enum class ETileType : uint8 {
+	Any UMETA(DisplayName = "Any"),
+	Earth UMETA(DisplayName = "Earth"),
+	Water UMETA(DisplayName = "Water")
+};
+
+
+USTRUCT(BlueprintType)
+struct FTRTileTypeTree : public FTableRowBase {
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	ETileType TileType;
+
+	UPROPERTY(EditAnywhere)
+	ETileType ParentType;
+
+};
+
+
+USTRUCT(BlueprintType)
+struct FTRTileType : public FTableRowBase {
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	int TileIndex;
+
+	UPROPERTY(EditAnywhere)
+	ETileType TileType;
+
 };
