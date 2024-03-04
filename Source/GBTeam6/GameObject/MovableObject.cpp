555cb60903a3173c088c3f24588e24658296f476
aksandez <aksandez@gmail.com>
2024-03-04 23:09:05 +0300

    changes: add SimpleObject; add MovableObject

---
+++ Source/GBTeam6/GameObject/MovableObject.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "./MovableObject.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
+// Sets default values
+AMovableObject::AMovableObject()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	
+	MovementComponent = CreateDefaultSubobject<UPawnMovementComponent, UFloatingPawnMovement>(TEXT("PawnMovementComponent"));
+	MovementComponent->SetUpdatedComponent(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AMovableObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
