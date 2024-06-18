d2f594454dd992eec58589913c739d824ffc7169
aksandez <aksandez@gmail.com>
2024-06-18 19:51:52 +0300

    changes: add basic logic to tower and baricade; forestlings can enter the tower; new basic hostile human AI; constructions not visible for characters fix

--- Source/GBTeam6/GameObject/SimpleObject.cpp
+++ Source/GBTeam6/GameObject/SimpleObject.cpp
@@ -77,3 +77,45 @@ void ASimpleObject::OnDeathBehaviour() {
 UGameObjectCore* ASimpleObject::GetCore_Implementation() {
 	return GameObjectCore;
 }
+
+UAISense_Sight::EVisibilityResult ASimpleObject::CanBeSeenFrom(const FCanBeSeenFromContext& Context,
+	FVector& OutSeenLocation, int32& OutNumberOfLoSChecksPerformed, int32& OutNumberOfAsyncLosCheckRequested,
+	float& OutSightStrength, int32* UserData, const FOnPendingVisibilityQueryProcessedDelegate* Delegate)
+{
+	FName StimuliSourceSightSocketName = FName(TEXT("StimuliSourceSightSocket"));
+	FTransform StimuliSourceTransform(GetActorLocation());
+	
+	if(ObjectMesh->DoesSocketExist(StimuliSourceSightSocketName)) {
+		StimuliSourceTransform = ObjectMesh->GetSocketTransform(StimuliSourceSightSocketName);
+    }
+
+    // Perform a line trace from the NPC's head to the player's head
+    FHitResult HitResult;
+    FCollisionQueryParams CollisionParams;
+    CollisionParams.AddIgnoredActor(Context.IgnoreActor);
+
+    bool bHit = GetWorld()->LineTraceSingleByChannel(
+    			HitResult,
+    			Context.ObserverLocation,
+    			StimuliSourceTransform.GetLocation(),
+    			ECC_Visibility,//ECC_GameTraceChannel1,
+    			CollisionParams
+    			);
+	
+    if (bHit)
+    {
+        //DrawDebugLine(GetWorld(), Context.ObserverLocation, StimuliSourceTransform.GetLocation(), FColor::Green, false, 0.1f, 0, 1.0f);
+    	OutSeenLocation = StimuliSourceTransform.GetLocation();
+    	OutNumberOfLoSChecksPerformed = 1;
+    	OutNumberOfAsyncLosCheckRequested = 0;
+    	OutSightStrength = 1.0f;
+    	return UAISense_Sight::EVisibilityResult::Visible;
+    }
+
+    //DrawDebugLine(GetWorld(), Context.ObserverLocation, StimuliSourceTransform.GetLocation(), FColor::Red, false, 0.1f, 0, 1.0f);
+    OutNumberOfLoSChecksPerformed = 1;
+    OutNumberOfAsyncLosCheckRequested = 0;
+    OutSightStrength = 0.0f;
+    return UAISense_Sight::EVisibilityResult::NotVisible;
+
+}
--- Source/GBTeam6/GameObject/SimpleObject.h
+++ Source/GBTeam6/GameObject/SimpleObject.h
@@ -7,13 +7,14 @@
 #include "../Component/Health/HealthBaseComponent.h"
 #include "../Interface/GameObjectInterface.h"
 #include "../Interface/GameObjectCore.h"
+#include "Perception/AISightTargetInterface.h"
 #include "Components/BoxComponent.h"
 #include "SimpleObject.generated.h"
 
 class UMappingDefaultComponent;
 
 UCLASS()
-class GBTEAM6_API ASimpleObject : public AActor, public IGameObjectInterface
+class GBTEAM6_API ASimpleObject : public AActor, public IGameObjectInterface, public IAISightTargetInterface
 {
 	GENERATED_BODY()
 	
@@ -63,5 +64,15 @@ protected:
 
 public:
 	virtual UGameObjectCore* GetCore_Implementation() override;
+
+	virtual UAISense_Sight::EVisibilityResult CanBeSeenFrom(
+		const FCanBeSeenFromContext& Context,
+		FVector& OutSeenLocation,
+		int32& OutNumberOfLoSChecksPerformed,
+		int32& OutNumberOfAsyncLosCheckRequested,
+		float& OutSightStrength,
+		int32* UserData = nullptr,
+		const FOnPendingVisibilityQueryProcessedDelegate* Delegate = nullptr
+		) override;
 };
 
