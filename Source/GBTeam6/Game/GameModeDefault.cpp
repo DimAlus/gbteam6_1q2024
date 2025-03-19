11b38a4209787ac5468f439e8f20ffa723eeabf0
DimAlus <dima.zvyagin.00@mail.ru>
2025-03-19 20:00:51 +0300

    changes: add console command system

--- Source/GBTeam6/Game/GameModeDefault.cpp
+++ Source/GBTeam6/Game/GameModeDefault.cpp
@@ -1,6 +1,170 @@
 #include "./GameModeDefault.h"
 #include "GBTeam6/Game/GameInstanceDefault.h"
+#include "GBTeam6/Player/PlayerPawnDefault.h"
+#include "Delegates/DelegateSignatureImpl.inl"
+#include <map>
 
-void AGameModeDefault::StartToLeaveMap() {
-	// Cast<UGameInstanceDefault>(GetGameInstance())->ClearServices();
+UGameInstanceDefault *AGameModeDefault::GetGameInstanceDefault() {
+	return Cast<UGameInstanceDefault>(GetGameInstance());
+}
+
+APlayerPawnDefault* AGameModeDefault::GetPlayerPawnDefault() {
+	return Cast<APlayerPawnDefault>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
+
+void AGameModeDefault::AddConsoleCommand(FName CommandName, FConsoleCommandBlueprintableSignature Callback) {
+	if (!DT_ConsoleCommands) {
+		return;
+	}
+	FTRConsoleCommandData* row = DT_ConsoleCommands->FindRow<FTRConsoleCommandData>(CommandName, "Command");
+	if (row) {
+		FConsoleCommandData data = (*row).Data;
+		data.Callback = Callback;
+		if (!data.CommandName.IsEmpty()) {
+			CommandsData.Push(data);
+		} else {
+			UE_LOG(LgGame, Error, TEXT("<%s>: Can't initialize Console command <%s>: invalid data!"), *GetNameSafe(this), *CommandName.ToString());
+		}
+	} else {
+		UE_LOG(LgGame, Error, TEXT("<%s>: Can't initialize Console command <%s>: not found at table!"), *GetNameSafe(this), *CommandName.ToString());
+	}
+}
+
+void AGameModeDefault::InitializeConsoleCommands() {
+	static std::map<EConsoleCommandArgumentType, void (*) (const FConsoleCommandArgumentInitializer&, const TArray<FString>&, TArray<FConsoleCommandParam>&, int&)> ArgumentParsers = {
+		{ EConsoleCommandArgumentType::Int, [](const FConsoleCommandArgumentInitializer& initializer, const TArray<FString>& Args, TArray<FConsoleCommandParam>& Params, int& Index) {
+			FConsoleCommandParam param;
+			param.IntValue = initializer.IntDefault;
+			if (!initializer.IsGhost && Index < Args.Num()) {
+				const FString& val = Args[Index++];
+				param.IntValue = FCString::Atoi(*val);
+			}
+			Params.Add(param);
+		}},
+
+		{ EConsoleCommandArgumentType::Float, [](const FConsoleCommandArgumentInitializer& initializer, const TArray<FString>& Args, TArray<FConsoleCommandParam>& Params, int& Index) {
+			FConsoleCommandParam param;
+			param.FloatValue = initializer.FloatDefault;
+			if (!initializer.IsGhost && Index < Args.Num()) {
+				const FString& val = Args[Index++];
+				param.FloatValue = FCString::Atof(*val);
+			}
+			Params.Add(param);
+		}},
+
+		{ EConsoleCommandArgumentType::String, [](const FConsoleCommandArgumentInitializer& initializer, const TArray<FString>& Args, TArray<FConsoleCommandParam>& Params, int& Index) {
+			FConsoleCommandParam param;
+			param.StringValue = initializer.StringDefault;
+			if (!initializer.IsGhost && Index < Args.Num()) {
+				const FString& val = Args[Index++];
+				param.StringValue = val;
+			}
+			Params.Add(param);
+		}},
+
+		{ EConsoleCommandArgumentType::Resource, [](const FConsoleCommandArgumentInitializer& initializer, const TArray<FString>& Args, TArray<FConsoleCommandParam>& Params, int& Index) {
+			FConsoleCommandParam param;
+			param.ResourceValue = initializer.ResourceDefault;
+			if (!initializer.IsGhost && Index < Args.Num()) {
+				TMap<FString, EResource> resmap = {
+					{ "wood", EResource::Wood },
+					{ "heal", EResource::Heal },
+				};
+				const FString& val = Args[Index++];
+				if (resmap.Contains(val.ToLower())) {
+					param.ResourceValue = resmap[val.ToLower()];
+				}
+			}
+			Params.Add(param);
+		}},
+
+		{ EConsoleCommandArgumentType::Class, [](const FConsoleCommandArgumentInitializer& initializer, const TArray<FString>& Args, TArray<FConsoleCommandParam>& Params, int& Index) {
+			FConsoleCommandParam param;
+			param.ClassValue = initializer.ClassDefault;
+			Params.Add(param);
+		}},
+	};
+
+
+	for (const auto& iter : RegisteredConsoleCommands) {
+		IConsoleManager::Get().UnregisterConsoleObject(iter.Value);
+	}
+	RegisteredConsoleCommands.Empty();
+	for (const FConsoleCommandData& data : CommandsData) {
+
+		RegisteredConsoleCommands.Add(data.CommandName, IConsoleManager::Get().RegisterConsoleCommand(
+			*data.CommandName,
+			*data.CommandDescription,
+			FConsoleCommandWithArgsDelegate::CreateLambda([=](const TArray<FString>& Args) {
+				TArray<FConsoleCommandParam> params;
+				int Index = 0;
+				for (const FConsoleCommandArgumentInitializer& init : data.Arguments) {
+					if (ArgumentParsers.find(init.Type) != ArgumentParsers.end()) {
+						ArgumentParsers[init.Type](init, Args, params, Index);
+						params[params.Num() - 1].Type = init.Type;
+					}
+				}
+				data.Callback.ExecuteIfBound(params);
+			}),
+			ECVF_Cheat
+		));
+
+
+
+
+
+
+		//FConsoleCommandWithArgsDelegate func = FConsoleCommandWithArgsDelegate::CreateUFunction(data.DelegateFloat.GetUObject(), data.DelegateFloat.GetFunctionName());
+		//func.BindUFunction(data.DelegateFloat.GetUObject(), data.DelegateFloat.GetFunctionName());
+		/*TObjectPtr<UObject> optr = this;
+		FConsoleCommandWithArgsDelegate dlgt;
+		bool found = true;*/
+
+		/*switch (data.CommandType)
+		{
+		case EConsoleCommandType::Float:
+			dlgt = FConsoleCommandWithArgsDelegate::CreateUFunction<UObject, float>(optr, data.DelegateFloat.GetFunctionName(), 2130.f);
+			dlgt.ExecuteIfBound({ "1.2" });
+			break;
+		case EConsoleCommandType::Int:
+			dlgt = FConsoleCommandWithArgsDelegate::CreateUFunction<UObject, int>(optr, data.DelegateInt.GetFunctionName(), 0);
+			break;
+		case EConsoleCommandType::StringArray:
+			
+			found = false;
+			break;
+		default:
+			found = false;
+			break;
+		}
+		if (found) {
+			RegisteredConsoleCommands.Add(data.CommandName, IConsoleManager::Get().RegisterConsoleCommand(
+				*data.CommandName,
+				*data.CommandDescription,
+				dlgt,
+				ECVF_Cheat
+			));
+
+		}*/
+
+		//IConsoleManager::Get().RegisterConsoleCommand(
+		//	*data.CommandName,
+		//	*data.CommandDescription,
+		//	FConsoleCommandWithWorldAndArgsDelegate::CreateLambda([=](const TArray<FString>& Args, UWorld* World) {
+		//		FTestSignature sig;
+		//		switch (data.CommandType)
+		//		{
+		//		case EConsoleCommandType::Touch:
+		//			sig.BindUFunction(data.DelegateFloat.GetUObject(), data.DelegateFloat.GetFunctionName());
+		//			sig.Execute(Args);
+		//			//data..ExecuteIfBound(Args);
+		//			break;
+		//		
+		//		default:
+		//			break;
+		//		}
+		//	}),
+		//	ECVF_Cheat
+		//);
+	}
 }
--- Source/GBTeam6/Game/GameModeDefault.h
+++ Source/GBTeam6/Game/GameModeDefault.h
@@ -3,10 +3,117 @@
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
 #include "GameFramework/GameModeBase.h"
+#include "HAL/IConsoleManager.h"
 
 #include "GBTeam6/Lib/Lib.h"
 
 #include "GameModeDefault.generated.h"
+
+class UGameInstanceDefault;
+class APlayerPawnDefault;
+
+
+UENUM(BlueprintType)
+enum class EConsoleCommandArgumentType : uint8 {
+	Int					UMETA(DisplayName = "Int"),
+	Float				UMETA(DisplayName = "Float"),
+	Resource			UMETA(DisplayName = "Resource"),
+	String				UMETA(DisplayName = "String"),
+	Class				UMETA(DisplayName = "Class"),
+};
+
+
+USTRUCT(BlueprintType)
+struct FConsoleCommandParam {
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EConsoleCommandArgumentType Type;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int IntValue{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float FloatValue{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString StringValue{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EResource ResourceValue{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AActor> ClassValue{};
+};
+
+DECLARE_DYNAMIC_DELEGATE_OneParam(FConsoleCommandBlueprintableSignature, const TArray<FConsoleCommandParam>&, Params);
+
+
+
+
+USTRUCT(BlueprintType)
+struct FConsoleCommandArgumentInitializer {
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsGhost{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EConsoleCommandArgumentType Type{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditCondition =
+		"Type == EConsoleCommandArgumentType::Int",
+		EditConditionHides))
+	int IntDefault{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditCondition =
+		"Type == EConsoleCommandArgumentType::Float",
+		EditConditionHides))
+	float FloatDefault{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditCondition =
+		"Type == EConsoleCommandArgumentType::String",
+		EditConditionHides))
+	FString StringDefault{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditCondition =
+		"Type == EConsoleCommandArgumentType::Resource",
+		EditConditionHides))
+	EResource ResourceDefault{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditCondition =
+		"Type == EConsoleCommandArgumentType::Class",
+		EditConditionHides))
+	TSubclassOf<AActor> ClassDefault{};
+};
+
+
+USTRUCT(BlueprintType)
+struct FConsoleCommandData {
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString CommandName{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString CommandDescription{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FConsoleCommandArgumentInitializer> Arguments{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FConsoleCommandBlueprintableSignature Callback;
+};
+
+
+USTRUCT(BlueprintType)
+struct FTRConsoleCommandData : public FTableRowBase {
+	GENERATED_USTRUCT_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FConsoleCommandData Data;
+};
+
 
 /** Default Game Mode for game
  * 
@@ -14,10 +121,29 @@
 UCLASS()
 class GBTEAM6_API AGameModeDefault : public AGameMode {
 	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = true)
+	UGameInstanceDefault* GetGameInstanceDefault();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = true)
+	APlayerPawnDefault* GetPlayerPawnDefault();
+
+private:
+	TMap<FString, IConsoleCommand*> RegisteredConsoleCommands;
+
+	TArray<FConsoleCommandWithArgsDelegate> Delegates;
+
+	TArray<FConsoleCommandData> CommandsData;
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Console")
+	UDataTable* DT_ConsoleCommands;
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FString SaveSlotName;
+	
+	UFUNCTION(BlueprintCallable, Category = "Console")
+	void AddConsoleCommand(FName CommandName, FConsoleCommandBlueprintableSignature Callback);
 
-	virtual void StartToLeaveMap() override;
+	UFUNCTION(BlueprintCallable, Category = "Console")
+	void InitializeConsoleCommands();
 };
